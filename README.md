8bd9b8f4305bfe28f610e5a971c7d37bc2c9dd15
BraxtonM <braxtoncoding@gmail.com>
2022-09-02 22:01:26 -0400

    Added README file, comments to code, and formatted code

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# UCBC5_EventCalendar
+# Braxton M | UCBC5_EventCalendar | README
+
+In this challenge, we were tasked with a list of acceptance criteria (featured below this section) to add a JavaScript file that would make a working Event / Work Day Calendar
+
+* We would use JavaScript, Jquery, and moment libraries
+* Display the current date and a timetable calendar below
+* The current, past, and present hours during the day will be highlighted in different colors
+* Add the ability to save events in the calendar and remain present locally when refreshed
+
+This project was given starter code.
+
+
+## Acceptance Criteria
+
+Here are the critical requirements necessary to develop a portfolio that satisfies a typical hiring manager’s needs:
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Screenshots
+
+Screenshots follow the order of the code and define sections 
+
+* Screenshot 1: Linking JavaScript File
+* ![App Screenshot](./Assets/README%20Screenshots/screenshot1.JPG)
+* Screenshot 2: Defining JavaScript Variables
+* ![App Screenshot](./Assets/README%20Screenshots/screenshot2.JPG)
+* Screenshot 3: Local Storage Variables
+* ![App Screenshot](./Assets/README%20Screenshots/screenshot3.JPG)
+* Screenshot 4: Adding Logic for displaying past, present, and current hours
+* ![App Screenshot](./Assets/README%20Screenshots/screenshot4.JPG)
+* Screenshot 5: function that changes the html and css classes of the timeslots and stores the variables locally even when the page is reloaded
+* ![App Screenshot](./Assets/README%20Screenshots/screenshot5.JPG)
+
+
+## Noteable Links
+
+ - [Github Profile](https://github.com/BrackyM)
+ - [Github Challenge 5 Repository](https://github.com/BrackyM/UCBC5_EventCalendar)
+ - [Challenge 5 Live Site](https://brackym.github.io/UCBC2_FirstPortfolioPage/)
+
+## Contributing Classmates
+
+Classmate that I collaborated with on completing the assignment and bug fixing for eachother
+
+- [Ishi Anand Github](https://github.com/ianad389)
--- script.js
+++ script.js
@@ -1,46 +1,49 @@
 // Constant that determines the current date / time of the user
-const currentDay = moment().format('MMMM Do YYYY, h:mm');
-const container = $(".container")
+const currentDay = moment().format("MMMM Do YYYY,");
+const container = $(".container");
 
-const currentHour = moment().hours()
+// Variable for the site to coorelate the current time block with the time displayed from their computer
+const currentHour = moment().hours();
 
-$("#currentDay").text(currentDay)
+$("#currentDay").text(currentDay);
 
 // Save Events
 
 for (let i = 8; i < 18; i++) {
-    let storageText = '';
-    let localStorageKey = 'hour-' + i;
+  let storageText = "";
+  let localStorageKey = "hour-" + i;
 
-    console.log(localStorageKey);
-    console.log(localStorage.getItem(localStorageKey));
+  console.log(localStorageKey);
+  console.log(localStorage.getItem(localStorageKey));
 
-    if (localStorage.getItem(localStorageKey)) {
-        storageText = localStorage.getItem(localStorageKey)
-    }
+  if (localStorage.getItem(localStorageKey)) {
+    storageText = localStorage.getItem(localStorageKey);
+  }
 
-    var hourClass
-    if(i < currentHour) {
-        hourClass = 'past'
-    } else if (i === currentHour) {
-        hourClass = 'current-event'
-    } else {
-        hourClass = 'future-event'
-    }
+  // Displays Past, Present, and Future time block with colored labels
 
+  var hourClass;
+  if (i < currentHour) {
+    hourClass = "past";
+  } else if (i === currentHour) {
+    hourClass = "current-event";
+  } else {
+    hourClass = "future-event";
+  }
 
-if (i < 13) {
+  // If Else statement identifies if 12 shows AM or PM time. Allows the events to be stored / saved / changed in the containers
+
+  if (i < 13) {
     container.append(`
     <div id="hour-${i}" class="row time-block"><div class="col-md-1 hour">
-    ${i}${i===12 ? "PM" : "AM"}
+    ${i}${i === 12 ? "PM" : "AM"}  
     </div>
     <textarea class="col-md-10 description ${hourClass}">${storageText}                                              
     </textarea>
     <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>             
     </div>                                                                                                    
-    `)
-}
-else{
+    `);
+  } else {
     container.append(`
     <div id="hour-${i}" class="row time-block"><div class="col-md-1 hour">
     ${i - 12}PM
@@ -49,21 +52,18 @@ else{
     </textarea>
     <button class="btn saveBtn col-md-1"><i class="fas fa-save"></i></button>             
     </div>
-    `)
+    `);
+  }
 }
-}
 
-const saveBtn = $(".saveBtn")
+// Save button functionality (locally stores events)
 
-$('.saveBtn').on('click', function () {
-    // finding nearby values
-    var value = $(this)
-        .siblings('.description')
-        .val();
-    var time = $(this)
-        .parent()
-        .attr('id');
+const saveBtn = $(".saveBtn");
 
-    localStorage.setItem(time, value);
+$(".saveBtn").on("click", function () {
+  // finding nearby values
+  var value = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+
+  localStorage.setItem(time, value);
 });
-
